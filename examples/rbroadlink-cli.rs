eff70eead812a03e29d0e6fba60a6733faad5ff4
Laco Skokan <skokys@gmail.com>
2024-05-28 19:32:21 +0200

    storing code to file when learning and blasting from file

--- examples/rbroadlink-cli.rs
+++ examples/rbroadlink-cli.rs
@@ -1,4 +1,6 @@
+use std::fs;
 use std::net::Ipv4Addr;
+use std::path::Path;
 
 use clap::{ArgEnum, Parser, Subcommand};
 use rpassword::read_password_from_tty;
@@ -26,6 +28,19 @@ enum Commands {
 
         /// The code to send, in hex (e.g. abcdef0123456789)
         code: String,
+    },
+
+    /// Blasts an IR / RF code to the world.
+    BlastFile {
+        /// Local IP of this machine. Use this if the broadlink device is on a different subnet.
+        #[clap(long, short)]
+        local_ip: Option<Ipv4Addr>,
+
+        /// The IP address of the broadlink device.
+        device_ip: Ipv4Addr,
+
+        /// filename where code is sored
+        store_file: String,
     },
 
     /// Connect a broadlink device to the network. Only tested on the RM3 Mini and the RM4 Pro
@@ -57,6 +72,10 @@ enum Commands {
         /// The type of code to learn
         #[clap(arg_enum)]
         code_type: LearnCodeType,
+
+        /// file where to sore code
+        #[clap(long,short)]
+        store_file: Option<String>
     },
 
     /// Lists available broadlink devices on the network
@@ -103,6 +122,11 @@ fn main() -> Result<(), String> {
             device_ip,
             code,
         } => blast(local_ip, device_ip, code),
+        Commands::BlastFile {
+            local_ip,
+            device_ip,
+            store_file,
+        } => blast_file(local_ip, device_ip, store_file),
         Commands::Connect {
             security_mode,
             ssid,
@@ -113,7 +137,8 @@ fn main() -> Result<(), String> {
             local_ip,
             device_ip,
             code_type,
-        } => learn(local_ip, device_ip, code_type),
+            store_file,
+        } => learn(local_ip, device_ip, code_type, store_file),
         Commands::List { local_ip } => list(local_ip),
         Commands::Info {
             local_ip,
@@ -125,6 +150,22 @@ fn main() -> Result<(), String> {
 fn blast(local_ip: Option<Ipv4Addr>, device_ip: Ipv4Addr, code: String) -> Result<(), String> {
     // Construct a device directly
     let device = Device::from_ip(device_ip, local_ip).expect("Could not connect to device!");
+    let hex_code = hex::decode(code).expect("Invalid code!");
+
+    // Ensure that the device is a remote
+    let remote = match device {
+        Device::Remote { remote } => remote,
+        _ => return Err("Device specified is not a remote!".into()),
+    };
+
+    println!("Blasting IR/RF code: {:02X?}", hex_code);
+    return remote.send_code(&hex_code);
+}
+
+fn blast_file(local_ip: Option<Ipv4Addr>, device_ip: Ipv4Addr, store_file: String) -> Result<(), String> {
+    // Construct a device directly
+    let device = Device::from_ip(device_ip, local_ip).expect("Could not connect to device!");
+    let code = fs::read_to_string(Path::new(&store_file)).expect("Unable to find or open store file");
     let hex_code = hex::decode(code).expect("Invalid code!");
 
     // Ensure that the device is a remote
@@ -180,6 +221,7 @@ fn learn(
     local_ip: Option<Ipv4Addr>,
     device_ip: Ipv4Addr,
     code_type: LearnCodeType,
+    write_file_name: Option<String>,
 ) -> Result<(), String> {
     println!("Attempting to learn a code of type {:?}...", code_type);
 
@@ -199,6 +241,9 @@ fn learn(
 
     let hex_string = hex::encode(&code);
     println!("Got code => {}", hex_string);
+    if let Some(write_file_name) = write_file_name {
+        fs::write(Path::new(&write_file_name), hex_string).expect("Unable to store code");
+    }
 
     return Ok(());
 }
